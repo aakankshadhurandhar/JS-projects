ef9e7f5aa96fad2df09e94394f0d110184e05125
aakankshadhurandhar <aakankshadhurandhar34@gmail.com>
2022-08-07 12:40:00 +0530

    feat:added on scroll progress bar

--- ProgressBar/src/js/script.js
+++ ProgressBar/src/js/script.js
@@ -1,20 +1,13 @@
-var flag=true;
-function move(){
-    if(flag){
-        flag=false
-        var width=10;
-        var elem=document.getElementById("progressbar") 
-        var progress=setInterval(frame,10)
-        function frame(){
-            if(width>=100){
-                clearInterval(progress)
-                flag=true
-            }
-            else{
-                width++
-                elem.style.width=width+"%"
-                elem.innerHTML=width+"%"
-            }
-        }
-    }
+function progressBarScroll(){
+    var scrollTop=document.documentElement.scrollTop
+    var height=document.documentElement.scrollHeight - document.documentElement.clientHeight
+    var width=(scrollTop/height)*100
+    var elem=document.getElementById("progressbar")
+    elem.style.width=width+"%"
+    elem.innerHTML=Math.floor(width)
 }
+window.onscroll=function(){
+    progressBarScroll()
+}
+
+
